2ef6e0be3aee7732ff114af0f61d3f4ad1a96599
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2023-12-18 14:26:24 +0000

    update all

--- js/gallery.js
+++ js/gallery.js
@@ -63,3 +63,11 @@ const images = [
       description: "Lighthouse Coast Sea",
     },
   ];
+
+  import * as basicLightbox from 'basiclightbox'
+
+const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`)
+
+instance.show()
